1873c7244b23c5e9db158b657e7529c7f3a41a48
aguswake1 <agustinolanoit@gmail.com>
2023-09-19 20:50:42 -0300

    realizo simulacro

--- 1 C/Introduccion a la Programacion/parcial/simulacro_parcial/test_simulacro.hs
+++ 1 C/Introduccion a la Programacion/parcial/simulacro_parcial/test_simulacro.hs
@@ -4,12 +4,12 @@ import Solucion
 main = runTestTT tests
 
 tests = test [
-    " relacionesValidas: una sola correcta" ~: (relacionesValidas [relacion1_2]) ~?= True,
-    " relacionesValidas: una sola incorrecta" ~: (relacionesValidas [relacion1_1]) ~?= False,
-    " personas: una sola relacion, 2 personas" ~: (sonIguales (personas [relacion1_2])  [usuario1, usuario2]) ~?= True,
-    " personas: dos relaciones, 3 personas" ~: (sonIguales (personas [relacion1_2, relacion1_3]) [usuario1, usuario2, usuario3]) ~?= True,
-    " amigosDe: una sola relacion, 1 solo amigo" ~: (amigosDe usuario1 [relacion1_2] ) ~?= [usuario2],
-    " personaConMasAmigos: 2 relaciones, 1 solo max" ~: (personaConMasAmigos [relacion1_2, relacion1_3]) ~?= usuario1
+    "relacionesValidas: una sola correcta" ~: (relacionesValidas [relacion1_2]) ~?= True,
+    "relacionesValidas: una sola incorrecta" ~: (relacionesValidas [relacion1_1]) ~?= False,
+    "personas: una sola relacion, 2 personas" ~: (sonIguales (personas [relacion1_2])  [usuario1, usuario2]) ~?= True,
+    "personas: dos relaciones, 3 personas" ~: (sonIguales (personas [relacion1_2, relacion1_3]) [usuario1, usuario2, usuario3]) ~?= True,
+    "amigosDe: una sola relacion, 1 solo amigo" ~: (amigosDe usuario1 [relacion1_2] ) ~?= [usuario2],
+    "personaConMasAmigos: 2 relaciones, 1 solo max" ~: (personaConMasAmigos [relacion1_2, relacion1_3]) ~?= usuario1
  ]
 
 -- Ejemplos
